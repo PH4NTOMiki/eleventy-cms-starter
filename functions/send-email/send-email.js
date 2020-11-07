dd39c6f4c6e835dc76f479c7300e533bd5962406
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2020-11-07 16:07:45 +0100

    Update send-email.js

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -1,5 +1,5 @@
 const SparkPost = require('sparkpost');
-const client = new SparkPost(process.env.SPARKPOST, {endpoint:'https://api.eu.sparkpost.com:443'});
+const client = new SparkPost(process.env.SPARKPOST, {origin:'https://api.eu.sparkpost.com:443'});
 
 exports.handler = function(event, context, callback) {
   client.transmissions
