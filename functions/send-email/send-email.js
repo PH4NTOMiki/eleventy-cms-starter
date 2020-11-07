f45e440ddad49a1f0faa71b2502cc22c0eb9a7f7
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2020-11-07 15:54:11 +0100

    Update send-email.js

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -11,5 +11,12 @@ exports.handler = function(event, context, callback) {
           "<html><body><p>My cool email. Testing</p></body></html>"
       },
     recipients: [{ address: 'mihaelmiki123@gmail.com' }]
+  }).then(data => {
+    console.log('Woohoo! You just sent your first mailing!');
+    console.log(data);
+  })
+  .catch(err => {
+    console.log('Whoops! Something went wrong');
+    console.log(err);
   });
 }
